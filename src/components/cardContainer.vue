36187706b62ccd99ea05b2fd56f6c2ad5dbcf3af
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-28 20:06:07 -0600

    Add gallery card to card container

---
+++ src/components/cardContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="CardContainer">
+    <h3>card container</h3>
+    <GalleryCard />
+  </div>
+</template>
+
+<script>
+import GalleryCard from './GalleryCard.vue'
+
+export default {
+  name: 'CardContainer',
+  components: {
+    GalleryCard
+  }
+}
+</script>
+
+<style scoped>
+
+#CardContainer {
+  display: grid;
+}
+
+
+h3 {
+  font-family: 'Fondamento', cursive;
+  color: #A51C31;
+  text-align:  center;
+  font-size: 1.5rem;
+}
+
+</style>
---
+++ src/components/galleryCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="GalleryCard">
+    <p>gallery card</p>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GalleryCard',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+#GalleryCard {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
