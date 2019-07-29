774e21af8fe834b9903d4b0388ed1c77642c7bac
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-29 16:56:47 -0600

    Add api to hidden file

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,28 @@
 
 <script>
 import CardContainer from './components/CardContainer.vue'
+import  key  from '../key.js'
+
+
+const url = "https://api.harvardartmuseums.org/image?apikey=" + key + "&size=50"
 
 export default {
   name: 'app',
   components: {
    CardContainer
   },
+  methods : {
+    fetchData() {
+      fetch(url)
+      .then(result =>  result.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+    }
+  },
+   beforeMount(){
+    this.fetchData()
+ },
+
   data() {
     return {
       gallery: []
